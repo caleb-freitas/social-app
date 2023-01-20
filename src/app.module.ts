e38bb63536cf503752b2428382ab562416cee73e
Caleb Freitas <calebfreitas@tutanota.com>
2023-01-20 13:07:18 -0300

    chore: added chat module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
-import { BullModule } from '@nestjs/bull';
 import { UserModule } from './app/user/user.module';
 import { PostModule } from './app/post/post.module';
+import { ChatModule } from './app/chat/chat.module';
 
 @Module({
   imports: [
@@ -11,14 +11,9 @@ import { PostModule } from './app/post/post.module';
       driver: ApolloDriver,
       autoSchemaFile: 'schema.graphql',
     }),
-    BullModule.forRoot({
-      redis: {
-        host: 'localhost',
-        port: 6379,
-      },
-    }),
     UserModule,
     PostModule,
+    ChatModule
   ],
 })
 export class AppModule {}
